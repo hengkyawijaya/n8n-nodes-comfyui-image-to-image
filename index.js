e44ddc3f581f0c53eb594b6dac2e95c372da6357
Hengky Wijaya <hengky.anang.wijaya@gmail.com>
2026-03-01 00:33:02 +0700

    feat: Update package version to 2.11.0 and remove unused ComfyUI nodes

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ module.exports = {
             nodePath: 'dist/nodes/ComfyUI/ComfyuiImageToImage.node.js',
             type: 'file',
         },
-        "comfyuiDualImageToImage": {
-            nodePath: 'dist/nodes/ComfyUI/ComfyuiDualImageToImage.node.js',
-            type: 'file',
-        },
         "comfyuiImageToVideo": {
             nodePath: 'dist/nodes/ComfyUI/ComfyuiImageToVideo.node.js',
-            type: 'file',
-        },
-        "comfyuiAudioGenerator": {
-            nodePath: 'dist/nodes/ComfyUI/ComfyuiAudioGenerator.node.js',
             type: 'file',
         }
     },
